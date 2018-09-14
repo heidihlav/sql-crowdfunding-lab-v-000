246048ad42593d3f1e5e121cca0d792d1f7b77c3
Heidi Hlavinka <hchlav@gmail.com>
2018-09-14 14:54:02 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -41,6 +41,7 @@ end
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
 "SELECT projects.category, SUM(pledges.amount)
 FROM projects
+INNER JOIN pledges
 ON projects.id = pledges.project_id
 GROUP BY projects.category;"
 end
